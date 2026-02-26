50005455e60a3bc5f76c4729e11aab3b3fdad05e
MD SHAHADAT HOSSAIN <m.shahadat.hossain.it@gmail.com>
2026-02-26 22:08:50 +0600

    wsgi.py

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -1,5 +1,11 @@
 import os
-from django.core.wsgi import get_wsgi_application
+import sys
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+path = '/home/mdshahadathossainit/research-hub/backend'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'core.settings'
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
